e1053f89b0cc5342eb371374a172ac4f9ff0b9d9
Kaustabh Mahanta <kaustabhworkspace@gmail.com>
2025-03-20 23:23:29 +0530

    refactor: optimize data fetching and enhance NoticeChart component

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -2,35 +2,33 @@ import Navbar from "./components/Navbar";
 import { auth } from "@clerk/nextjs/server";
 import { PrismaClient } from "@prisma/client";
 import { redirect } from "next/navigation";
-import { Toaster } from 'react-hot-toast';
-const prisma = new PrismaClient()
+import { Toaster } from "react-hot-toast";
+const prisma = new PrismaClient();
 export default async function AdminLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    const { userId } = await auth()
-    if (!userId) {
-        redirect('/sign-in')
-    }
-    const findAdmin = await prisma.admin.findUnique({
-        where: {
-            clerkId: userId
-        }
-    })
-    if (!findAdmin) {
-        redirect('/register-admin')
-    }
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/sign-in");
+  }
+  const findAdmin = await prisma.admin.findUnique({
+    where: {
+      clerkId: userId,
+    },
+  });
+  if (!findAdmin) {
+    redirect("/register-admin");
+  }
 
-    return (
-        <>
-            <Navbar />
-            <div className="p-5 sm:p-10 w-svw">
-                {children}
-                <Toaster position="top-center"
-                    reverseOrder={false} />
-            </div>
-        </>
-
-    );
+  return (
+    <>
+      <Navbar />
+      <div className="p-5 sm:p-10 w-svw">
+        {children}
+        <Toaster position="top-center" reverseOrder={false} />
+      </div>
+    </>
+  );
 }
